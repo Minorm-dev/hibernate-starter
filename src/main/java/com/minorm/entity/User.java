16096f2d62a6194febd0adbd5d13362e5c2a7bf4
Minorm-dev <maxim.san231@gmail.com>
2025-03-05 00:11:36 +0300

    Inheritance

--- src/main/java/com/minorm/entity/User.java
+++ src/main/java/com/minorm/entity/User.java
@@ -20,7 +20,8 @@ import java.util.Set;
 @ToString(exclude = {"company", "profile", "userChats"})
 @Entity
 @Table(name = "users", schema = "public")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type")
 public abstract class User implements Comparable<User>, BaseEntity<Long> {
 //
 //    @Id
